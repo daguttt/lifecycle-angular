abc6b5db54b569d35e117cec2bec8616fb4e1605
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-27 17:28:03 -0500

    Class 296: Project init - lifecycle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { ShowNameComponent } from './components/show-name/show-name.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Page1Component, ShowNameComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/show-name/show-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-name',
+  templateUrl: './show-name.component.html',
+  styleUrls: ['./show-name.component.css']
+})
+export class ShowNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
